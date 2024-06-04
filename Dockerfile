0ced308dc262ec70df2d0af7c94bedb1bf6b0a6b
Hannah Gordon <hannahgordon@sky.com>
2024-06-04 20:18:17 +0100

    Enable multiple workers within the container

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.11
 
-FROM python:3.9
+COPY ./requirements.txt /app/requirements.txt
 
-WORKDIR /code
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
-COPY ./requirements.txt /code/requirements.txt
-
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-
-COPY ./app /code/app
-
-CMD ["fastapi", "run", "app/main.py", "--port", "80"]
+COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
@@ -9,7 +9,17 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
+def write_file(file_id: int):
+    with open("{}.txt".format(file_id), mode="w") as f:
+        content = f"hi"
+        f.write(content)
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/makefile/{file_id}")
+async def make_a_file(file_id: int, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_file, file_id)
+    return {"message": "{} started".format(file_id)}
